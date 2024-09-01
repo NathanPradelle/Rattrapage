f8710aad2b003b06ada9373022fdd06fd0060040
NathanPradelle <nathan.pradelle77@gmail.com>
2024-09-01 16:09:22 +0200

    NavBar welcome

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import { Head, Link } from '@inertiajs/react';
 
-import Navbar from '@/Components/Navbar';
+import Navbar from '@/Components/Navbar/NavBar.jsx';
 
 const Welcome = ({ auth }) => {
   const handleImageError = () => {
